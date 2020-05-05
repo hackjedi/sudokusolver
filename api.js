815cdedbb57d7fc1c8ce63f1618c8241297ab474
hackjedi <alexanderdm@gmail.com>
2020-05-05 23:27:41 +0200

    removed /sudoku

--- api.js
+++ api.js
@@ -5,10 +5,13 @@ const app = express()
 app.get('/hello', function (req, res) {
     res.send('Hello World')
 })
-app.get('/sudoku', function (req, res) {
-   let grid =  solveSudoku(req.body.grid)
+
+/*app.get('/sudoku', function (req, res) {
+    let grid;
+    grid = solveSudoku(req.body.grid);
     res.send(grid)
 })
+*/
 
 app.listen(3000)
 
